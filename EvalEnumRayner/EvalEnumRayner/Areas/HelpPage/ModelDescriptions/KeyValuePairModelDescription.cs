86c0057875d76b038c439addf230ad9813f997eb
Rayner Jimmy Tapia Caballero <a2019116944@estudiantes.upsa.edu.bo>
2021-03-19 13:14:48 -0700

    Creacion de Proyecto EvalEnum

---
+++ EvalEnumRayner/EvalEnumRayner/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace EvalEnumRayner.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ EvalEnumRayner/EvalEnumRayner/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EvalEnumRayner.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
